730d61c528159e6e34ae677c11b675d852bef328
YimiaoOu <32688730+YimiaoOu@users.noreply.github.com>
2021-04-27 18:06:38 -0400

    Add cache extension methods and change feature in the Razor page

---
+++ RedisDemoApp/RedisExtensions/DistributedCacheExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement.Internal;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace RedisDemoApp.RedisExtensions
+{
+    public static class DistributedCacheExtensions
+    {
+        // Extension Method
+        public static async Task SetEntryAsync<T>(this IDistributedCache cache,
+            string id,
+            T data,
+            TimeSpan? absoluteExpireTime = null,
+            TimeSpan? idleExpireTime = null)
+        {
+            var options = new DistributedCacheEntryOptions();
+            options.AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromSeconds(120);
+            options.SlidingExpiration = idleExpireTime;
+
+            var jsonData = JsonSerializer.Serialize(data);
+
+            await cache.SetStringAsync(id, jsonData, options);
+        }
+
+
+        // Extension Method
+        public static async Task<T> GetEntryAsync<T>(this IDistributedCache cache, string id)
+        {
+            var jsonData = await cache.GetStringAsync(id);
+
+            if (jsonData is null)
+            {
+                return default(T);
+            }
+
+            return JsonSerializer.Deserialize<T>(jsonData);
+        }
+    }
+}
